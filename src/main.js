57d0b70952474797bc7de9795e88ce63fbddf228
Andrew Hird <andrew.james.hird@gmail.com>
2016-11-22 18:45:09 +1300

    time entry component written

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Home from './components/Home.vue'
+import TimeEntries from './components/TimeEntries.vue'
 
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
@@ -16,6 +17,9 @@ const router = new VueRouter()
 router.map({
   '/home': {
     component: Home
+  },
+  '/time-entries': {
+    component: TimeEntries
   }
 })
 
